57f58751f378ff77277757f25bf153c94e05d16d
merytpeters <merytpeters@gmail.com>
2025-06-12 16:26:47 +0100

    Refactor: Add user profile route with authentication handling

---
+++ backend/app/routes/user/__init__.py
@@ -0,0 +1,8 @@
+"""Individual User Routes"""
+from .profile import router as profile_router
+
+
+routers = [
+    profile_router,
+]
+
---
+++ backend/app/routes/user/profile.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from backend.app.schemas.user import (
+    UserOut
+)
+from backend.lib.errorlib.auth import UserNotAuthorizedException
+from backend.app.services.user_service import (
+    get_current_user_from_db as profile_service
+)
+from backend.app.dependencies.security import user_oauth2_scheme
+from backend.app.db.session import get_db
+
+
+router = APIRouter()
+
+
+@router.get("/profile", response_model=UserOut)
+def profile(
+    db: Session = Depends(get_db),
+    token: str = Depends(user_oauth2_scheme)
+):
+    """User Profile"""
+    try:
+        user, _ = profile_service(token, db)
+        return user
+    except UserNotAuthorizedException as e:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
