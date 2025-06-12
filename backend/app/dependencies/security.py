0081bdf826e4a0c4590d6e18c76fdd91e7020bba
merytpeters <merytpeters@gmail.com>
2025-06-12 16:26:09 +0100

    Refactor: Add authentication security module for protected routes

---
+++ backend/app/dependencies/security.py
@@ -0,0 +1,5 @@
+"""Auth Security For Protected Routes"""
+from fastapi.security import OAuth2PasswordBearer
+
+user_oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/user/login")
+# admin_oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/admin/login")
