3edfd62f4b951903c2f1996de3db141566dcc073
merytpeters <merytpeters@gmail.com>
2025-07-14 01:47:44 +0100

    Refactor: Enhance CommonUserDashboard with profile picture and update styles for better layout

--- frontend/healthaid-frontend/src/components/userDashboards/commonuserdashboard.tsx
+++ frontend/healthaid-frontend/src/components/userDashboards/commonuserdashboard.tsx
@@ -1,10 +1,14 @@
 import '../../styles/userdashboards/commonuserdashboard.css';
+import { FaUserCircle } from "react-icons/fa";
 
 function CommonUserDashboard  () {
     return (
         <div className="commonuserdashboard">
             <section className="section1">
                 Personal Info
+                <aside className="profilepic">
+                    <FaUserCircle size={30}/>
+                </aside>
             </section>
             <section className="section2">
                 Bio Data
